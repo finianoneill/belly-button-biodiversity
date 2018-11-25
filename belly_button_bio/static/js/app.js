346996485adc0c08269482182a1272432cfa47ea
Finian O'Neill <finian.d.oneill@gmail.com>
2018-11-25 12:13:02 -0800

    Remove test js file

--- belly_button_bio/static/js/app.js
+++ belly_button_bio/static/js/app.js
@@ -41,13 +41,32 @@ function buildCharts(sample) {
     }];
     var BUBBLE = document.getElementById('bubble');
     Plotly.newPlot(BUBBLE, bubbleData, bubbleLayout);
-  })
-
-    
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+
+    console.log(sampleData[0]['sample_values'].slice(0, 10))
+    var pieData = [{
+        values: sampleData[0]['sample_values'].slice(0, 10),
+        labels: sampleData[0]['otu_ids'].slice(0, 10),
+        hovertext: labels.slice(0, 10),
+        hoverinfo: 'hovertext',
+        type: 'pie'
+    }];
+    var pieLayout = {
+        margin: { t: 0, l: 0 }
+    };
+    var PIE = document.getElementById('pie');
+    Plotly.plot(PIE, pieData, pieLayout);
+    
+
+  })
+
+    
+
+
+    
 }
 
 function init() {
