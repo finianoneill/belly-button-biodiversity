db3abfaaabc8b66b6ee26a95476821bdf98f4931
Finian O'Neill <finian.d.oneill@gmail.com>
2018-11-25 12:50:42 -0800

    Add metadata display

--- belly_button_bio/static/js/app.js
+++ belly_button_bio/static/js/app.js
@@ -3,21 +3,33 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  d3.json(`/metadata/${sample}`).then(function(metaData){
+
+    console.log(metaData);
     // Use d3 to select the panel with id of `#sample-metadata`
+    var panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
+    panel.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    Object.entries(metaData).forEach((entry) => {
+        const [key, value] = entry;
+        panel
+          .append("option")
+          .text(`${key}: ${value}`)
+          .property("value", key);
+    });
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+  });
 }
 
 function buildCharts(sample) {
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  //Plotly.d3.json(`/samples/${sample}`, function(error, sampleData) {
   d3.json(`/samples/${sample}`).then(function(sampleData){
     // @TODO: Build a Bubble Chart using the sample data
     var bubbleLabels = sampleData["otu_ids"];
